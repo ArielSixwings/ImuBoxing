65f22669f43f4fd7819ea03372301a57c1ce9adb
ArielSixwings <arielserafim@gmail.com>
2024-03-30 14:53:02 -0300

    [Telemetry] Add StartStreaming method

--- Telemetry/src/ImuNode.cpp
+++ Telemetry/src/ImuNode.cpp
@@ -1,5 +1,8 @@
 #include "ImuNode.h"
 
+#include <sstream>
+#include <thread>
+
 ImuNode::ImuNode() : Node("imuNode"), m_io() 
 {
     try {
@@ -8,6 +11,9 @@ ImuNode::ImuNode() : Node("imuNode"), m_io()
         RCLCPP_ERROR(this->get_logger(), "Failed to open serial port: %s", e.what());
         throw;
     }
+
+    m_startService = this->create_service<std_srvs::srv::Empty>(
+        "imu/start", std::bind(&ImuNode::StartStreaming, this, _1, _2));
 
     auto timerCallback = [this]() -> void {
         // Placeholder for timer callback functionality
@@ -19,4 +25,73 @@ ImuNode::ImuNode() : Node("imuNode"), m_io()
 void ImuNode::CreateAnglesPublisher()
 {
     m_publisher = this->create_publisher<geometry_msgs::msg::Vector3>("imu/angles", 1);
+}
+
+void ImuNode::StartStreaming([[maybe_unused]] const std::shared_ptr<std_srvs::srv::Empty::Request> request,
+                             [[maybe_unused]] std::shared_ptr<std_srvs::srv::Empty::Response> response)  
+{
+    if (m_streaming) {
+        RCLCPP_WARN(this->get_logger(), "Already streaming");
+
+        return;
+    }
+
+    RCLCPP_INFO(this->get_logger(), "Start Streaming");
+
+    std::vector<int> imuNumbers = {3};
+    int commandValue = 85;
+
+    for (auto id : imuNumbers) {
+        auto command = CreateImuCommand(id, commandValue);
+        ApplyCommand(command);
+    }
+
+    m_streaming = true;
+}
+
+std::string ImuNode::CreateImuCommand(int logicalId,
+                                      int commandNumber,
+                                      const std::vector<int>& arguments ) 
+{
+
+    std::ostringstream command;
+
+    command << ">" << logicalId << "," << commandNumber;
+
+    if (not arguments.empty()) {
+        
+        command << ",";
+
+        for (size_t i = 0; i < arguments.size(); ++i) {
+            command << arguments[i];
+            if (i < arguments.size() - 1) {
+                command << ",";
+            }
+        }
+    }
+
+    command << "\n";
+    return command.str();
+}
+
+void ImuNode::ApplyCommand(const std::string& command, bool showResponse) 
+{
+    m_serialPort->write_some(boost::asio::buffer(command));
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    if (showResponse) 
+    {
+        std::vector<char> response_buffer(128); // Adjust size as needed
+
+        size_t bytes_read = m_serialPort->read_some(boost::asio::buffer(response_buffer));
+
+        if (bytes_read > 0) 
+        {
+            std::string response(response_buffer.begin(), response_buffer.begin() + bytes_read);
+            RCLCPP_INFO(this->get_logger(), ">> %s", response.c_str());
+        }
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
--- Telemetry/src/ImuNode.h
+++ Telemetry/src/ImuNode.h
@@ -14,11 +14,22 @@ public:
 
     void CreateAnglesPublisher();
 
+    void StartStreaming(const std::shared_ptr<std_srvs::srv::Empty::Request> request,
+                        std::shared_ptr<std_srvs::srv::Empty::Response> response);
+
+    void ApplyCommand(const std::string& command, bool show_response = false);
+
+    static std::string CreateImuCommand( int logicalId,
+                                         int commandNumber,
+                                         const std::vector<int>& arguments = {});
+
+
 private:
     int imuNumber = 3;
-    bool streaming = false;
+    bool m_streaming = false;
     rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr m_publisher;
     std::shared_ptr<boost::asio::serial_port> m_serialPort;
     boost::asio::io_service m_io; // Declare io object
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr m_startService;
     rclcpp::TimerBase::SharedPtr m_timer;
 };

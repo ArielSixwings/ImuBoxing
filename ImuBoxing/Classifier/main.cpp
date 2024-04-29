e09d76c54dc4302e3ccb75c7539b3bec2e6f6799
ArielSixwings <arielserafim@gmail.com>
2024-04-29 09:39:59 -0300

    [Classifier] acquire pose data.

--- ImuBoxing/Classifier/main.cpp
+++ ImuBoxing/Classifier/main.cpp
@@ -6,12 +6,12 @@ int main(int argc, char *argv[])
 
     rclcpp::init(argc, argv);
 
-    const std::string fileName = path + "dataTest.csv";
+    // const std::string fileName = path + "dataTest.csv";
     // const std::string fileName = path + "guard.csv";
     // const std::string fileName = path + "jabEnd.csv";
     // const std::string fileName = path + "hookEnd.csv";
-    // const std::string fileName = path + "uppercutEnd.csv";
-    rclcpp::spin(std::make_shared<classifier::FeatureExtractor>(fileName, 2048, true));
+    const std::string fileName = path + "uppercutEnd.csv";
+    rclcpp::spin(std::make_shared<classifier::FeatureExtractor>(fileName, 2048, false));
     rclcpp::shutdown();
 
     return 0;
